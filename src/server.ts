9215c1560dd2681943848d719f354525c27bf78e
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-07-22 00:31:12 +0530

    chore: apis

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import { HttpServer, express } from "http-rest-api"
 import cors from "cors"
 import { configs } from "./configs"
+import { apis } from "./apis"
 
 export default async function server() {
 	console.log(`Server is starting...`)
-	const app = new HttpServer(configs.server.port)
 
+	const app = new HttpServer(configs.server.port)
 	app.use(cors())
 	app.use(express.json())
 	app.use(express.urlencoded({ extended: true }))
 
-	app.listen().then((port) => console.log(`Server started on port: ${port}`))
+	app.registerRestApis(...apis)
+	app.listen().then((port) =>
+		console.log(
+			`Server started on port ${port} in ${configs.server.environment} mode`,
+		),
+	)
 
 	return app
 }
